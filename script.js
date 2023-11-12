a23da0ff13442e538afb3c4a755c48bc846123fc
srilekha yadav maraboini <150052882+MaraboiniSrilekha@users.noreply.github.com>
2023-11-12 22:59:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+console.log("welcome guys")
+var num1=3
+var num2=4
+var num3=8
+console.log(num1+num2)
+
+
+i_favourate_food="pizza"
+myNativePlace="kurnool"
+numOfBirds=27
+
+ var num2=8
+console.log(num1+num2)
+
+console.log(num1*num2)
+console.log(num3/num2)
+
+num4=4
+num4++
+console.log(num4)
+num4--
+console.log(num4)
+
+let num5=4
+   num5=num5+6
+   console.log(num5)
+
+   num6=5
+   num6 +=7
+   console.log(num6)
+   num6=5
+   num6 *=4
+   console.log(num6)
+   num6=5
+   num6 =num6*4
+   console.log(num6)
+
+   let jack=0
+   let john=2
+   console.log(jack==jack)
+    
+   let siri=11
+  let mythu="11"
+  console.log(siri===mythu)
+   
+   let num8=6
+   let num9=7
+   console.log(num8!=num9)
+
+    let num22=3
+    let num23=4
+    console.log(num22<4 && num23>2)
+    let num44=8
+    let num66=55
+    console.log(num44=8||num>=56)
+    let num43=67
+    let num65=89
+    console.log(!(num43=67&&num65>80))
+
+  function dance(params) {
+    console.log("dance is an exercise")
+  }
+  dance()
+  function muitiplication(num23,num24,num35) {
+    console.log("num23*num24*num25") 
+  }
+  muitiplication(4,5,6)
